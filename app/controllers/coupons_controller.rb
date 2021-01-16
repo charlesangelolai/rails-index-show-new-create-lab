a1556eea34af956165cf6d356f0c6d73958cb374
Charles Lai <charlesangeloplai@gmail.com>
2021-01-16 00:17:43 -0500

    commit tree f88cf08221c1dc5d864dbcf4dd45506355911f1f

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,27 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    find_coupon
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
+
+    redirect_to coupon_path(@coupon)
+  end
+
+  private
+    def find_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+end
